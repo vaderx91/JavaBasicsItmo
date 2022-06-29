4178306d6bc45d572e7776c03e8c7f2016b774e2
Антон Кузнецов <kuznetsovanthony@yandex.ru>
2022-06-29 18:11:50 +0300

    lesson2

--- src/JavaBasics/lesson2/lab2/Calculator.java
+++ src/JavaBasics/lesson2/lab2/Calculator.java
@@ -1,25 +1,43 @@
 package JavaBasics.lesson2.lab2;
 
 public class Calculator {
-    public static void main(String[] args) {
-    }
+
     // Сумма
     public static Double sum(Double a, Double b) { return a + b;}
     public static Integer sum(Integer a, Integer b) {return a + b;}
     public static Long sum(Long a, Long b) {return a + b;}
 
     //Вычитание
-    public static Double substraction(Double a, Double b) {return a - b;}
-    public static Integer substraction(Integer a, Integer b) {return a - b;}
-    public static Long substraction(Long a, Long b) {return a - b;}
+    public static Double sub(Double a, Double b) {return a - b;}
+    public static Integer sub(Integer a, Integer b) {return a - b;}
+    public static Long sub(Long a, Long b) {return a - b;}
 
     //Умножение
-    public static Double multiplicator(Double a, Double b) {return a * b;}
-    public static Integer multiplicator(Integer a, Integer b) {return a * b;}
-    public static Long multiplicator(Long a, Long b) {return a * b;}
+    public static Double multi(Double a, Double b) {return a * b;}
+    public static Integer multi(Integer a, Integer b) {return a * b;}
+    public static Long multi(Long a, Long b) {return a * b;}
 
     //Деление
-    public static Double division(Double a, Double b) {return (double) a / b;}
-    public static Integer division(Integer a, Integer b) {return a / b;}
-    public static Long division(Long a, Long b) {return a / b;}
+    public static Double div(Double a, Double b) {return (double) a / b;}
+    public static Double div(Integer a, Double b) {return (double) a / b;}
+    public static Double div(Long a, Long b) {return (double) (a / b);}
+    public static void main(String[] args) {
+        System.out.println(Calculator.sum(1.2, 2.4));
+        System.out.println(Calculator.sum(1, 2));
+        System.out.println(Calculator.sum(12L, 24L));
+
+        System.out.println(Calculator.sub(4.3, 2.5));
+        System.out.println(Calculator.sub(3, 1));
+        System.out.println(Calculator.sub(10L, 3L));
+
+        System.out.println(Calculator.multi(4.3, 2.5));
+        System.out.println(Calculator.multi(3, 1));
+        System.out.println(Calculator.multi(10L, 3L));
+
+        System.out.println(Calculator.div(4.3, 2.5));
+        System.out.println(Calculator.div(3,1d));
+        System.out.println(Calculator.div(10L, 3L));
+
+    }
+
 }
