a0cd5b5ddeefa591adb1878af4d2b1300dfc49ac
Антон Кузнецов <kuznetsovanthony@yandex.ru>
2022-06-30 12:21:01 +0300

    lesson3.t2

--- src/JavaBasics/lesson3/lab3/t2/MainCar.java
+++ src/JavaBasics/lesson3/lab3/t2/MainCar.java
@@ -7,7 +7,7 @@ public class MainCar {
         Car car1 = new Car();
         car1.setColor(String.valueOf(Color.GREEN));
         car1.setName("Mini Cooper");
-        car1.setWeight(1440.0);
+        car1.setWeight(1429.0);
         System.out.println(car1);
 
         Car car2 = new Car(Color.BLACK);
